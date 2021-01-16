4d4d9e66237916f480e4eaa42c8f41bf5bed4509
mahesh <chambharem7@gmail.com>
2021-01-16 12:14:43 +0530

    done

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -1,100 +1,168 @@
 import { Node } from "./node.js";
 
-class LinkList {
-  //! Iniatializing the Linked-List
-
+class LinkedList {
   constructor() {
     this.head = null;
     this.size = 0;
   }
 
-  //   ! Adding Element in linked-list
-
-  add(data) {
-    let node = new Node(data);
-
-    let current;
-
-    //? when linked list is empty then make new node head
+  // ! Adding element to link list
+  add(element) {
+    let node = new Node(element);
 
     if (this.head == null) {
+      // ? if the linked list has no node then asign first node
       this.head = node;
     } else {
-      current = this.head;
-
-      while (current.next) {
-        current = current.next;
-      }
-
-      current.next = node;
-    }
-
-    this.size++;
-  }
-  // ! Add element at Index
-  addAtIndex(data, index) {
-    let node = new Node(data);
-    let curr, prev;
-
-    curr = this.head;
-
-    //   ? When Index  zero
-
-    if (index == 0) {
       node.next = this.head;
       this.head = node;
-    } else {
-      let val = 0;
-      while (val < index) {
-        val++;
-        prev = curr;
-        curr = curr.next;
-      }
-      node.next = curr;
-      prev.next = node;
     }
-    this.size++;
+    return this.size++;
   }
 
-  //   ! Remove element from index
-  remove(index) {
-    // ? if index is invalid
+  //  ! Insert Element At Index
+
+  insertAtIndex(element, index) {
     if (index > 0 && index > this.size) {
-      console.log("Enter Valid Index for removing element");
       return -1;
     } else {
+      let node = new Node(element);
       let curr;
-      let prev;
       let val = 0;
-
+      let prev = curr;
       curr = this.head;
-      prev = curr;
-      if (index === 0) {
-        this.head = curr.next;
+      if (index == 0) {
+        this.head = node;
+        node.next = this.head;
       } else {
-        // ? Looping through link list for getting the index no
         while (val < index) {
           val++;
           prev = curr;
           curr = curr.next;
         }
 
+        prev.next = node;
+        node.next = curr;
+      }
+      return this.size++;
+    }
+  }
+
+  // !  Remove element using index
+
+  remove(index) {
+    if (index < 0 && index < this.size) {
+      return -1;
+    } else {
+      let curr = this.head;
+      let prev;
+      if (index == 0) {
+        this.head = curr.next;
+      } else {
+        let val = 0;
+        while (val < index) {
+          val++;
+          prev = curr;
+          curr = curr.next;
+        }
         prev.next = curr.next;
       }
-      this.size--;
-      return curr.element;
+      return this.size--;
     }
+  }
+
+  // ! Remove element using element
+
+  removeEle(element) {
+    let curr = this.head;
+    let prev = null;
+    while (curr != null) {
+      if (curr.element === element) {
+        if (prev == null) {
+          this.head = curr.next;
+        } else {
+          prev.next = curr.next;
+        }
+      }
+      prev = curr;
+      curr = curr.next;
+    }
+    console.log("Element Not Found for romoval");
+    return -1;
+  }
+
+  indexOf(element) {
+    let curr = this.head;
+    let count = 0;
+
+    while (curr) {
+      if (curr.element == element) {
+        console.log("index of " + element + " is " + count);
+        return count;
+      }
+      count++;
+      curr = curr.next;
+    }
+    console.log(element + " Not Valid");
+
+    return -1;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  sizeOf() {
+    return console.log("Size of Linked List is " + this.size);
+  }
+
+  print() {
+    let str = "";
+    let curr = this.head;
+
+    while (curr) {
+      str += curr.element + " ";
+      curr = curr.next;
+    }
+    return console.log(str);
   }
 }
 
-let LL = new LinkList();
+let ll = new LinkedList();
 
-LL.add(10);
-LL.add(20);
-LL.add(30);
-LL.add(40);
-LL.add(50);
-LL.addAtIndex(55, 5);
-LL.addAtIndex(1, 0);
-LL.remove(10);
-console.log(LL);
+ll.add(10);
+ll.add(20);
+ll.add(30);
+ll.add(40);
+ll.add(50);
+ll.insertAtIndex(15, 2);
+ll.remove(2);
+ll.removeEle(400);
+ll.indexOf(10);
+ll.sizeOf();
+console.log(ll.isEmpty());
+ll.print();
+console.log(ll);
+
+// ! Output
+
+/* 
+Element Not Found for romoval
+linked-list.js:92 index of 10 is 4
+linked-list.js:108 Size of Linked List is 5
+linked-list.js:135 false
+linked-list.js:119 50 40 30 20 10 
+linked-list.js:137 
+LinkedList {head: Node, size: 5}
+head: Node
+element: 50
+next: Node
+element: 40
+next: Node
+element: 30
+next: Node
+element: 20
+next: Node
+element: 10
+next: null
+*/
